905a079316d2697552ed51e959f6e9bc213befb2
DaniloKuwai <danilorkuwai@gmail.com>
2022-07-12 22:38:55 -0300

    aula01 - variaveis e tipo - variaveis

---
+++ variaves e tipos/aula01 - Variáveis/script.js
@@ -0,0 +1,11 @@
+//var -> funciona no escopo global
+//let -> funciona no escopo de bloco (por exemplo dentro de uma função)
+
+
+numberOne = 1;
+
+console.log(numberOne + 2);
+
+//a variável pode ser trocado o valor
+//const não pode ser trocado o valor
+//let só funciona no escopo, por exemplo em uma função
