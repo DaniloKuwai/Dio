fc43762c3913ca6acb06a5af3ba3a922871a6932
DaniloKuwai <danilorkuwai@gmail.com>
2022-07-16 21:55:05 -0300

    Finalizacao desafios

--- variaves e tipos/desafio palindromo/script.js
+++ variaves e tipos/desafio palindromo/script.js
@@ -1,9 +1,27 @@
 /*O que é um palíndromo, é uma frase que lendo ela de trás para frente ela tem o mesmo sentido*/
 
+//Solução nº 1
 function verificaPalindromo(string){
     if(!string) return;
 
-    string.split("").reverse().join === string;
+    return string.split("").reverse().join("") === string;
     /*o reverse vai reverter o array*/
     /*o join junta todas string do array*/
 }
+
+console.log(verificaPalindromo("ovo"));
+
+//Solução 2
+
+function verificaPalindromo2(string){
+    if(!string) return "string inexistente"
+
+    for(let i = 0; i <string.length / 2; i++){
+        if(string[i] !== string[string.length -1 -i]){
+            return false
+        }
+    }
+    return true
+}
+
+console.log(verificaPalindromo2("gato"))
